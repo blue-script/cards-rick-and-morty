d65aea579b8dcc1614af27bd5f9bf8d390125f81
blue-script <alexm92@inbox.ru>
2025-02-09 18:45:56 +0500

    feat: add SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
+import s from "./App.module.css"
+import {SearchBar} from "./components/SearchBar"
 import {useState} from "react"
-import "./App.css"
+
+export type DataType = {
+  info: {
+    "count": number;
+    "pages": number;
+    "next": string;
+    "prev": string;
+  },
+  result: ItemsType;
+}
+
+export type ItemsType = ItemType[];
+
+export type ItemType = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: ItemsOrigin;
+  location: ItemLocation;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export type ItemsOrigin = {
+  name: string;
+  url: string;
+}
+
+export type ItemLocation = {
+  name: string;
+  url: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<DataType | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  console.log(data)
 
   return (
-    <>
-      hello
-    </>
+    <main className={s["main"]}>
+      <div className={s["main__container"]}>
+        <div className={s["main__search"]}>
+          <SearchBar setData={setData} setLoading={setLoading} data={data}/>
+        </div>
+        <div className={s["main__content"]}>
+
+        </div>
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,47 @@
+import {useState, useEffect} from "react"
+import {DataType} from "../../App"
+import cn from "classnames"
+import s from "./search-bar.module.css"
+
+type SearchBarProps = {
+  setData: (data: DataType | null) => void
+  setLoading: (value: boolean) => void
+  data: DataType | null
+}
+
+export const SearchBar = ({setData, setLoading, data}: SearchBarProps) => {
+  const [query, setQuery] = useState("")
+
+  useEffect(() => {
+    if (query.length < 4) {
+      setData(null)
+      return
+    }
+
+    setLoading(true)
+
+    fetch(`https://rickandmortyapi.com/api/character/?name=${query.toLowerCase()}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+      })
+      .catch((err) => console.error("Ошибка загрузки:", err))
+      .finally(() => setLoading(false))
+  }, [query])
+
+  return (
+    <div className={s["search-bar"]}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search characters..."
+        className={s["search-bar__input"]}
+        autoFocus
+      />
+      <span className={cn(s["search-bar__info"], {[s["search-bar__info-hidden"]]: data === null})}>
+        {`Found characters: ${data?.info?.count || 0}`}
+      </span>
+    </div>
+  )
+}
